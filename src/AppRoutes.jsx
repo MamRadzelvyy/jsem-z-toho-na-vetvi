11aded9b7bbc28f99197f6ec27f00edda7596c7c
Jan Rylich <rylich.honza@email.cz>
2025-02-24 15:13:22 +0100

    Dan má raka

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
+import Danmáraka from "./Danmaraka";
 
 export default function AppRoutes() {
   return (
@@ -7,6 +8,7 @@ export default function AppRoutes() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/rip" element={<Danmáraka />} />
         </Routes>
       </BrowserRouter>
     </>
