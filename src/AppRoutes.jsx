99882efd12eb87b5b843716e35d5c44ab3580c4c
ondrejfilip1 <ondrej.filip@student.spsmb.cz>
2025-02-24 15:14:02 +0100

    asdasd

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
+import Product from "./Product";
 
 export default function AppRoutes() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} />
         </Routes>
       </BrowserRouter>
     </>
