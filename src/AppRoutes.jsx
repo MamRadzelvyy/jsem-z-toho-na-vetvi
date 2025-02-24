ee1ac2e5f0758759e1ba0d9b0eff3018dd0361ba
Jan Vais <114480538+Vaisaz@users.noreply.github.com>
2025-02-24 15:16:58 +0100

    Update AppRoutes.jsx

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Photos from "./photos";
+import Contact from "./cont";
 
 export default function AppRoutes() {
   return (
@@ -9,6 +10,7 @@ export default function AppRoutes() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/photos" element={<Photos />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
     </>
